5026a5b75186619e4c828cdbda78fd7cf2541d8e
Jgwentworth <jaredgeorge.us@gmail.com>
2017-07-13 14:19:59 -0700

    added signin page

--- projects/pet-find-boot/main.py
+++ projects/pet-find-boot/main.py
@@ -1,17 +1,32 @@
-from flask import Flask, request, redirect, render_template
+from flask import Flask, request, redirect, render_template, session
+from flask_sqlalchemy import SQLAlchemy
 import cgi
 
 app = Flask(__name__)
-
 app.config['DEBUG'] = True
+app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://pet-find-boot:fhyBwcgZHHGiMj53@localhost:8889/pet-find-boot"
+app.config["SQLALCHEMY_ECHO"] = True
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120))
+    email = db.Column(db.String(120))
+    password = db.Column(db.String(255))
+
+    def __init__(self, name, email, password):
+        self.name = name
+        self.email = email
+        self.password = password
+
 
 @app.route("/")
 def home_page():
     return render_template("index.html")
 
-@app.route("/sign-in")
+@app.route("/sign-up")
 def display_index():
-    return render_template("sign-in.html")
+    return render_template("sign-up.html")
 
 def is_blank(resp):
     if len(resp) == 0:
@@ -35,13 +50,13 @@ def valid_email(resp):
     else:
         return False
 
-def sign_in():
-    return render_template("sign-in.html")
+def sign_up():
+    return render_template("sign-up.html")
 
-@app.route("/sign-in", methods=["POST"])
+@app.route("/sign-up", methods=["POST"])
 def validate_response():
 
-    username = request.form["username"]
+    name = request.form["username"]
     password = request.form["password"]
     password2 = request.form["password2"]
     email = request.form["email"]
@@ -53,12 +68,12 @@ def validate_response():
     pass_error2 = ''
     email_error = ''
 
-    if is_blank(username):
+    if is_blank(name):
         name_error = "Empty field"
     else:    
-        if not is_valid(username):
+        if not is_valid(name):
             name_error = not_valid
-            username = ""
+            name = ""
     if is_blank(password):
         pass_error = "Empty field"
     else:
@@ -78,13 +93,14 @@ def validate_response():
             email_error = "Not valid email"
             email = ""        
     if not name_error and not pass_error and not pass_error2 and not email_error:
-        name = username
-        return render_template("welcome.html",
-            name = name
-        )
+        user_id = User(name, email, password)
+        db.session.add(user_id)
+        db.session.commit()
+        session['username'] = name
+        return redirect("/")
     else:
-        return render_template("sign-in.html",
-            username = username,
+        return render_template("sign-up.html",
+            username = name,
             password = password,
             password2 = password2,
             email = email,
@@ -92,7 +108,11 @@ def validate_response():
             pass_error = pass_error,
             pass_error2 = pass_error2,
             email_error = email_error
-    )             
+    )  
+@app.route("/sign-in")
+def sign_in():
+    return render_template("sign-in.html")    
+
 @app.route("/search")
 def search_page():
     return render_template("search.html")
@@ -101,4 +121,5 @@ def search_page():
 def post_page():
     return render_template("post.html")    
 
-app.run()    
+if __name__ == "__main__" :
+    app.run()    
