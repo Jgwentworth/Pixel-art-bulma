69226a6a14453d30722b0de1d660f0893874b7b6
Jared George <jaredgeorge.us@gmail.com>
2017-08-05 13:57:55 -0700

    switched to bulma css

---
+++ projects/pixel-art-bulma/index.js
@@ -0,0 +1,62 @@
+$(document).ready(makeBox())
+
+function makeBox(){
+    for (i = 0; i < 4902; i++){ 
+        let $gridBox = $("<div></div>").css({"width": "10px", 
+                "height": "10px",
+                "outline": "1px solid gray",
+                "background-color": "white",
+                "float": "left"});
+        $gridBox.attr("id", i)
+        $gridBox.attr("class", "grid")
+                 
+        $("#pixel-container").append($gridBox);                              
+        }
+};
+
+let colorHold = "black";
+let mouseDown = false;
+
+$(".color").change(function(){
+    colorHold = this.value;
+    $("#current-color").css("background-color", colorHold);
+    return colorHold
+});
+ 
+$(".grid").click(function(){
+$(this).css({"background-color": colorHold,
+                "outline": colorHold});
+})
+.mousedown(function(){
+    mouseDown = true;
+
+})
+.mouseup(function(){
+    mouseDown = false;
+
+})
+.mouseenter(function(){
+    if (mouseDown == true){
+    $(this).css({"background-color": colorHold,
+                "outline": colorHold}); 
+        return
+    } else {
+    return                         
+    }          
+})
+.dblclick(function(){
+    colorHold = $(this).css('background-color');
+    console.log(colorHold)
+
+
+});
+
+           
+           
+
+
+            
+
+
+
+
